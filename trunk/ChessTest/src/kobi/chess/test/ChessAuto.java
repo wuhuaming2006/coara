df1ee1ebe28636a166350c667f5900b6a79dd4d0
hauserns <hauserns@d4cfb8a7-1580-29a3-0469-09d5b55630d5>
2014-05-18 21:55:32 +0000

    improvement for uiautomator to open app added README

--- trunk/ChessTest/src/kobi/chess/test/ChessAuto.java
+++ trunk/ChessTest/src/kobi/chess/test/ChessAuto.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
@@ -49,7 +50,7 @@ public class ChessAuto extends UiAutomatorTestCase {
 		UiDevice d = getUiDevice();
 		d.pressHome();
 		//open chess
-		d.click(421,  492);
+		launchApp("Pocket Chess For Android");
 		sleep(1000);
 	}
 
@@ -168,4 +169,38 @@ public class ChessAuto extends UiAutomatorTestCase {
 		d.drag(204, 593, 185, 661, 1);
 		waitUntilMyTurn();
 	}
+	
+	protected static void launchApp(String nameOfAppToLaunch) throws UiObjectNotFoundException {
+        UiScrollable appViews = new UiScrollable(new UiSelector().scrollable(true));
+          // Set the swiping mode to horizontal (the default is vertical)
+          appViews.setAsHorizontalList();
+          appViews.scrollToBeginning(10);  // Otherwise the Apps may be on a later page of apps.
+          int maxSearchSwipes = appViews.getMaxSearchSwipes();
+
+          UiSelector selector;
+          selector = new UiSelector().className(android.widget.TextView.class.getName());
+          
+          UiObject appToLaunch;
+          
+          // The following loop is to workaround a bug in Android 4.2.2 which
+          // fails to scroll more than once into view.
+          for (int i = 0; i < maxSearchSwipes; i++) {
+
+              try {
+                  appToLaunch = appViews.getChildByText(selector, nameOfAppToLaunch);
+                  if (appToLaunch != null) {
+                      // Create a UiSelector to find the Settings app and simulate      
+                      // a user click to launch the app.
+                      appToLaunch.clickAndWaitForNewWindow();
+                      break;
+                  }
+              } catch (UiObjectNotFoundException e) {
+                  System.out.println("Did not find match for " + e.getLocalizedMessage());
+              }
+
+                  appViews.scrollForward();
+                  System.out.println("scrolling forward 1 page of apps.");
+          }
+    }
+	
 }
